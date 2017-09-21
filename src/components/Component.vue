ac377d26af41bb58590869ac7b2b7fced048183f
diableJDY <okonghpp@naver.com>
2017-09-21 13:49:22 +0900

    vue add 추가

---
+++ src/components/Component.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="component">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links111</h2>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'component',
+    data () {
+      return {
+        msg: '뻐큐머겅5555511111111111111444444441'
+      }
+    }
+  }
+</script>
+
+
+<!--
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
+-->
